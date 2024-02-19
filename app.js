ef8ba9795ab78e2c185de4e65ba05f7575e75f13
Akash BG <akashbg4@gmail.com>
2024-02-19 20:07:52 +0530

    add search feature

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 const apiKey = "0c42b7b1aabd8766e38ca140097c091a";
-const city = "Tokyo";
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-const data = import("./response");
-async function fetchWeather() {
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+
+function setWeatherIcon(icon) {
+  if (icon) {
+    document.querySelector(".weather-icon").src = icon;
+  }
+}
+
+async function fetchWeather(url) {
   const response = await fetch(url);
   const data = await response.json();
 
-  console.log(data);
+  if (!data?.main) {
+    document.querySelector(".weather").style.display = "none";
+    return;
+  }
+
+  document.querySelector(".weather").style.display = "block";
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  switch (data.weather[0].main) {
+    case "Clear":
+      setWeatherIcon("images/clear.png");
+      break;
+    case "Clouds":
+      setWeatherIcon("images/clouds.png");
+      break;
+    case "Drizzle":
+      setWeatherIcon("images/drizzle.png");
+      break;
+    case "Mist":
+      setWeatherIcon("images/mist.png");
+      break;
+    case "Rain":
+      setWeatherIcon("images/rain.png");
+      break;
+    case "Snow":
+      setWeatherIcon("images/snow.png");
+      break;
+  }
 }
 
-fetchWeather();
+searchButton.addEventListener("click", () => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchBox.value}&appid=${apiKey}&units=metric`;
+  fetchWeather(url);
+});
